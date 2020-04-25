1f84fd6dbd4515fb6f3a5340780e1f95f6bee2bd
ckf48 <3048764886@qq.com>
2020-04-25 17:56:40 +0800

    Initial commit

---
+++ system/src/main/java/com/storm/system/SystemApplication.java
@@ -0,0 +1,14 @@
+package com.storm.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SystemApplication.class, args);
+    }
+}
