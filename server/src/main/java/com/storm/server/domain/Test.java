25207a98c3a49b27f3367293a8a2371be0293693
ckf48 <3048764886@qq.com>
2020-04-28 12:28:22 +0800

    1.试图集成mybatis generator 插件。失败。转向linux系统

--- server/src/main/java/com/storm/server/domain/Test.java
+++ server/src/main/java/com/storm/server/domain/Test.java
@@ -1,27 +1,14 @@
 package com.storm.server.domain;
 
+import lombok.Data;
+
 /**
  * @author ckf48
  */
+@Data
 public class Test {
 
     private String id;
 
     private String name;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
